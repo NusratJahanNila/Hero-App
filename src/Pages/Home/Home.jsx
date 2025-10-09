1055524b457f21d441c67b83d02321f5dbd824b3
NusratJahanNila <nnusratjahan330@gmail.com>
2025-10-10 00:07:37 +0600

    added loader to the Home Page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Banner from '../../Components/Header/Banner';
 import TrendingApps from '../../Components/FeaturedApps/TrendingApps';
+import useApps from '../../Components/Hooks/useApps';
+import Spinner from '../../Components/Spinner/Spinner';
 
 const Home = () => {
+    const { loading } = useApps();
     return (
 
-        <div className='bg-gray-100'>
-            <Banner></Banner>
-            <TrendingApps></TrendingApps>
+        <div>
+            {
+                loading ? <Spinner></Spinner> : <div className='bg-gray-100'>
+                    <Banner></Banner>
+                    <TrendingApps></TrendingApps>
+                </div>
+            }
         </div>
     );
 };
